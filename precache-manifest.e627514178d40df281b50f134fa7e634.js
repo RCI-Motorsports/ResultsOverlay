14d0037c80051a6705f1c321410cb9067e706542
Mikael Sundström <mikael@sundstrom.st>
2020-10-11 13:22:46 +0200

    Updates

---
+++ precache-manifest.e627514178d40df281b50f134fa7e634.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d0a283f7583ec71a313f388f6c2d31",
+    "url": "/ResultsOverlay/index.html"
+  },
+  {
+    "revision": "e84ba8afc0b21a43800e",
+    "url": "/ResultsOverlay/static/css/main.0c540967.chunk.css"
+  },
+  {
+    "revision": "f85003edd06604290476",
+    "url": "/ResultsOverlay/static/js/2.c439d94a.chunk.js"
+  },
+  {
+    "revision": "756782bd9a9399326fc70fcabd08ef3f",
+    "url": "/ResultsOverlay/static/js/2.c439d94a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84ba8afc0b21a43800e",
+    "url": "/ResultsOverlay/static/js/main.900ff55c.chunk.js"
+  },
+  {
+    "revision": "ae1a3cdcdcb41b9fe08f",
+    "url": "/ResultsOverlay/static/js/runtime-main.dfcb15a1.js"
+  },
+  {
+    "revision": "d31bab048696f0127959d9321bd337cb",
+    "url": "/ResultsOverlay/static/media/Champ_Standings_Slide_AM.d31bab04.svg"
+  },
+  {
+    "revision": "0c65b33f27bc69e4822fa0ffb4abdffc",
+    "url": "/ResultsOverlay/static/media/Champ_Standings_Slide_PRO.0c65b33f.svg"
+  },
+  {
+    "revision": "1c4707fb516ddadace3fd20782486ce4",
+    "url": "/ResultsOverlay/static/media/Champ_Standings_Slide_ROOKIE.1c4707fb.svg"
+  },
+  {
+    "revision": "edda9cd495543007e8f915910c7148b5",
+    "url": "/ResultsOverlay/static/media/Champ_Standings_Slide_SILVER.edda9cd4.svg"
+  },
+  {
+    "revision": "588cc981f128b60a3220b3f9f366d2ff",
+    "url": "/ResultsOverlay/static/media/Standing_Title.588cc981.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ResultsOverlay/static/media/logo.5d5d9eef.svg"
+  }
+]);
