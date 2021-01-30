ae7ec13847bc1ae319f64461cc759c3539562fe7
Mikael Sundström <mikael@sundstrom.st>
2021-01-30 16:59:26 +0100

    Add country flags

---
+++ src/Countries.js
@@ -0,0 +1,190 @@
+import Afghanistan from "./img/countries/Afghanistan.png";
+import Andorra from "./img/countries/Andorra.png";
+import Argentina from "./img/countries/Argentina.png";
+import Armenia from "./img/countries/Armenia.png";
+import Australia from "./img/countries/Australia.png";
+import Austria from "./img/countries/Austria.png";
+import Azerbaijan from "./img/countries/Azerbaijan.png";
+import Bahrain from "./img/countries/Bahrain.png";
+import Barbados from "./img/countries/Barbados.png";
+import Belgium from "./img/countries/Belgium.png";
+import BosniaAndHerzegovina from "./img/countries/Bosnia & Herzegovina.png";
+import Brazil from "./img/countries/Brazil.png";
+import Bulgaria from "./img/countries/Bulgaria.png";
+import Canada from "./img/countries/Canada.png";
+import Chile from "./img/countries/Chile.png";
+import China from "./img/countries/China.png";
+import Colombia from "./img/countries/Colombia.png";
+import Croatia from "./img/countries/Croatia.png";
+import Cuba from "./img/countries/Cuba.png";
+import Czechia from "./img/countries/Czech Republic.png";
+import Denmark from "./img/countries/Denmark.png";
+import DominicanRepublic from "./img/countries/Dominican Republic.png";
+import Egypt from "./img/countries/Egypt.png";
+import England from "./img/countries/England.png";
+import Estonia from "./img/countries/Estonia.png";
+import European from "./img/countries/European.png";
+import Finland from "./img/countries/Finland.png";
+import France from "./img/countries/France.png";
+import Georgia from "./img/countries/Georgia.png";
+import Germany from "./img/countries/Germany.png";
+import Greece from "./img/countries/Greece.png";
+import HongKong from "./img/countries/Hong Kong.png";
+import Hungary from "./img/countries/Hungary.png";
+import India from "./img/countries/India.png";
+import Indonesia from "./img/countries/Indonesia.png";
+import International from "./img/countries/International.png";
+import Iran from "./img/countries/Iran.png";
+import Ireland from "./img/countries/Ireland.png";
+import Israel from "./img/countries/Israel.png";
+import Italy from "./img/countries/Italy.png";
+import Jamaica from "./img/countries/Jamaica.png";
+import Japan from "./img/countries/Japan.png";
+import Kuwait from "./img/countries/Kuwait.png";
+import Latvia from "./img/countries/Latvia.png";
+import Lebanon from "./img/countries/Lebanon.png";
+import Lithuania from "./img/countries/Lithuania.png";
+import Luxembourg from "./img/countries/Luxembourg.png";
+import Macau from "./img/countries/Macau.png";
+import Madagascar from "./img/countries/Madagascar.png";
+import Malaysia from "./img/countries/Malaysia.png";
+import Malta from "./img/countries/Malta.png";
+import Monaco from "./img/countries/Monaco.png";
+import Morocco from "./img/countries/Morocco.png";
+import Nepal from "./img/countries/Nepal.png";
+import Netherlands from "./img/countries/Netherlands.png";
+import NewCaledonia from "./img/countries/New Caledonia.png";
+import NewZealand from "./img/countries/New Zealand.png";
+import Nigeria from "./img/countries/Nigeria.png";
+import NorthernIreland from "./img/countries/Northern Ireland.png";
+import Norway from "./img/countries/Norway.png";
+import Oman from "./img/countries/Oman.png";
+import PapuaNewGuinea from "./img/countries/Papua New Guinea.png";
+import Philippines from "./img/countries/Philippines.png";
+import Poland from "./img/countries/Poland.png";
+import Portugal from "./img/countries/Portugal.png";
+import PuertoRico from "./img/countries/Puerto Rico.png";
+import Qatar from "./img/countries/Qatar.png";
+import Romania from "./img/countries/Romania.png";
+import Russia from "./img/countries/Russia.png";
+import SanMarino from "./img/countries/San Marino.png";
+import SaudiArabia from "./img/countries/Saudi Arabia.png";
+import Scotland from "./img/countries/Scotland.png";
+import Serbia from "./img/countries/Serbia.png";
+import Singapore from "./img/countries/Singapore.png";
+import Slovakia from "./img/countries/Slovakia.png";
+import Slovenia from "./img/countries/Slovenia.png";
+import SouthAfrica from "./img/countries/South Africa.png";
+import SouthKorea from "./img/countries/South Korea.png";
+import Spain from "./img/countries/Spain.png";
+import Sweden from "./img/countries/Sweden.png";
+import Switzerland from "./img/countries/Switzerland.png";
+import Taiwan from "./img/countries/Taiwan.png";
+import Thailand from "./img/countries/Thailand.png";
+import Tunisia from "./img/countries/Tunisia.png";
+import Turkey from "./img/countries/Turkey.png";
+import TurksAndCaicos from "./img/countries/Turks & Caicos.png";
+import Ukraine from "./img/countries/Ukraine.png";
+import UnitedArabEmirates from "./img/countries/United Arab Emirates.png";
+import UnitedKingdom from "./img/countries/United Kingdom.png";
+import Uruguay from "./img/countries/Uruguay.png";
+import USA from "./img/countries/USA.png";
+import Venezuela from "./img/countries/Venezuela.png";
+import Wales from "./img/countries/Wales.png";
+import Zimbabwe from "./img/countries/Zimbabwe.png";
+
+export default { Afghanistan,
+    Andorra,
+    Argentina,
+    Armenia,
+    Australia,
+    Austria,
+    Azerbaijan,
+    Bahrain,
+    Barbados,
+    Belgium,
+    BosniaAndHerzegovina,
+    Brazil,
+    Bulgaria,
+    Canada,
+    Chile,
+    China,
+    Colombia,
+    Croatia,
+    Cuba,
+    Czechia,
+    Denmark,
+    DominicanRepublic,
+    Egypt,
+    England,
+    Estonia,
+    European,
+    Finland,
+    France,
+    Georgia,
+    Germany,
+    Greece,
+    HongKong,
+    Hungary,
+    India,
+    Indonesia,
+    International,
+    Iran,
+    Ireland,
+    Israel,
+    Italy,
+    Jamaica,
+    Japan,
+    Kuwait,
+    Latvia,
+    Lebanon,
+    Lithuania,
+    Luxembourg,
+    Macau,
+    Madagascar,
+    Malaysia,
+    Malta,
+    Monaco,
+    Morocco,
+    Nepal,
+    Netherlands,
+    NewCaledonia,
+    NewZealand,
+    Nigeria,
+    NorthernIreland,
+    Norway,
+    Oman,
+    PapuaNewGuinea,
+    Philippines,
+    Poland,
+    Portugal,
+    PuertoRico,
+    Qatar,
+    Romania,
+    Russia,
+    SanMarino,
+    SaudiArabia,
+    Scotland,
+    Serbia,
+    Singapore,
+    Slovakia,
+    Slovenia,
+    SouthAfrica,
+    SouthKorea,
+    Spain,
+    Sweden,
+    Switzerland,
+    Taiwan,
+    Thailand,
+    Tunisia,
+    Turkey,
+    TurksAndCaicos,
+    Ukraine,
+    UnitedArabEmirates,
+    UnitedKingdom,
+    Uruguay,
+    USA,
+    Venezuela,
+    Wales,
+    Zimbabwe 
+};
